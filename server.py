b0ddb112d3275eb81f44d75af8f88b61c1b4b9c5
Svetlana Esina <magistresina@gmail.com>
2019-07-27 23:56:52 +0300

    fix typo

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/reminder-lesson", methods=['POST'])
 def lesson():
     data = request.get_json()
-    print data
+    print(data)
 
 if __name__ == "__main__":
     app.run(port=PORT)
