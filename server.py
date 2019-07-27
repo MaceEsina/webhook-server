e0dd9f8281f0da4ef677b647042befef67009437
Svetlana Esina <magistresina@gmail.com>
2019-07-28 01:44:59 +0300

    process post request

--- server.py
+++ server.py
@@ -3,10 +3,12 @@ from flask import Flask, request
 PORT = 9000
 app = Flask(__name__)
 
-@app.route("/reminder-lesson", methods=['POST'])
-def lesson():
+@app.route('/reminder-lesson', methods=['POST'])
+def webhook():
+    print('POST request')
     data = request.get_json()
-    print(data)
+    print('data:', data)
+    return '', 200
 
 if __name__ == "__main__":
-    app.run(port=PORT)
+    app.run(host='0.0.0.0', port=PORT)
