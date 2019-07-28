b9973a7e7b2c8dba46a7fc6d36f369062d5cd689
Svetlana Esina <magistresina@gmail.com>
2019-07-29 02:07:56 +0300

    fix webhook data processing

--- server.py
+++ server.py
@@ -1,22 +1,23 @@
 from flask import Flask, request
 from utils import getToken, getCustomer, sendMessage
+import json
 
 PORT = 9000
 app = Flask(__name__)
 
 @app.route('/reminder-lesson', methods=['POST'])
 def webhook():
-  response = request.get_json()
-  customerIds = response['customer_ids']
+  data = request.get_json()
+  print('Webhook data:', data)
+  customerIds = data['customer_ids']
 
   for id in customerIds:
     token = getToken()
     if token:
       customer = getCustomer(token, id)
-      sendMessage(customer, response)
+      sendMessage(customer, data)
       print('Customer info:', customer)
 
-  print('Webhook response data:', response)
   return '', 200
 
 if __name__ == "__main__":
