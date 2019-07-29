75effcf17534307f8c97648ff33bbcb308b42edb
Svetlana Esina <magistresina@gmail.com>
2019-07-29 19:44:59 +0300

    deploy

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
 from utils import getToken, getCustomer, sendMessage
+from flask import Flask, request
+from waitress import serve
 
 PORT = 9000
 app = Flask(__name__)
@@ -7,7 +8,7 @@ app = Flask(__name__)
 @app.route('/reminder-lesson', methods=['POST'])
 def lessonWebhook():
   data = request.get_json()
-  print('Webhook data:', data)
+  # print('Webhook data:', data)
   customerIds = data['fields_new']['customer_ids']
 
   for id in customerIds:
@@ -15,9 +16,8 @@ def lessonWebhook():
     if token:
       customer = getCustomer(token, id)
       sendMessage(customer, data)
-      print('Customer info:', customer)
+      # print('Customer info:', customer)
 
   return '', 200
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=PORT)
+serve(app, host='0.0.0.0', port=PORT)
